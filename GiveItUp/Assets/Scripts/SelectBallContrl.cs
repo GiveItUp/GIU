4e025321f4c86020b4edfc81a8ec3813055c9a30
zengfei <zeng.fei@monstar-lab.com>
2015-05-15 21:53:19 +0800

    点击章鱼2，替换为章鱼1.

--- GiveItUp/Assets/Scripts/SelectBallContrl.cs
+++ GiveItUp/Assets/Scripts/SelectBallContrl.cs
@@ -33,47 +33,25 @@ public class SelectBallContrl : MonoBehaviour {
 	void Update () {
 	
 	}
-	float y =0;
 	void OnBtn_Start_Input (ref POINTER_INFO ptr)
 	{
-		if(ptr.inputDelta.y>0)
-			return;
 		switch (ptr.evt) {
-		case POINTER_INFO.INPUT_EVENT.DRAG:
-			y += ptr.inputDelta.y;
-			ChangeSacle();
-			break;
-		case POINTER_INFO.INPUT_EVENT.RELEASE_OFF:
 		case POINTER_INFO.INPUT_EVENT.RELEASE:
-			y = 0;
-			ResetOrderbyBalls();
+		case POINTER_INFO.INPUT_EVENT.TAP:
+			if (!((UIButton)(ptr.targetObj)).IsReleaseEnabled)
+				return;
+			ChangeSacle(ptr.targetObj.gameObject.GetComponent<UIButton>());
+			
 			break;
 		}
+
 	}
 
-	void ChangeSacle()
+	void ChangeSacle(UIButton temp)
 	{
-		for (int i = 1; i < balls.Count; i++) {
-
-			UIButton item = balls[i];
-			item.transform.parent.localScale += new Vector3(0.008f,0.008f,0);
-			balls[0].transform.parent.localScale -= new Vector3(0.008f,0.008f,0);
-		}
-
-		for (int i = 1; i < balls.Count; i++) {
-
-			UIButton item = balls[i];
-			item.transform.parent.localPosition += new Vector3(4f/i+1f*(i-1),0,0);
-			if(i == 1 && item.transform.parent.localPosition.x >0)
-			{
-				UIButton temp = balls[0];
-				balls.Remove(temp);
-				balls.Add(temp);
-				ResetOrderbyBalls();
-				return;
-			}
-			balls[0].transform.parent.localPosition += new Vector3(0.5f,0,1);
-		}
+		balls.Remove(temp);
+		balls.Insert(0,temp);
+		ResetOrderbyBalls();
 	}
 
 	void ResetOrderbyBalls()
