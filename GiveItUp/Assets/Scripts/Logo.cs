8d3c028204dfb9ea48c0658c9bba63ffcfcfee50
Roger <jia.luo@monstar-lab.com>
2015-05-19 19:02:46 +0800

    加入logo场景

---
+++ GiveItUp/Assets/Scripts/Logo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo : MonoBehaviour
+{
+	GameObject go;
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		go = GameObject.Instantiate (Resources.Load ("Prefabs/Monstar/logo1")) as GameObject;
+		yield return new WaitForSeconds (2);
+		Destroy (go);
+		go = GameObject.Instantiate (Resources.Load ("Prefabs/Monstar/logo2")) as GameObject;
+		yield return new WaitForSeconds (2);
+		Destroy (go);
+		go = GameObject.Instantiate (Resources.Load ("Prefabs/Monstar/logo3")) as GameObject;
+		yield return new WaitForSeconds (2);
+		Application.LoadLevel (1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
